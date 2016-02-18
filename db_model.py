49ed9852f8d9b78d83a23385b693f66410740f65
jnwcki <jude.nowicki@gmail.com>
2016-02-17 22:37:04 -0500

    added checking for duplicate usernames when adding

--- db_model.py
+++ db_model.py
@@ -4,24 +4,19 @@ class DBReader:
         self.name = name
         self.password = password
         self.account = self.get_user(name, password)
-
+        self.user_data = None
+        self.status_report = ''
     def read_file(self):
         with open("login_db") as infile:
             return [line.split(',') for line in infile.readlines()]
 
     def get_user(self, name, password):
-        my_read_file = self.read_file()
-        print(my_read_file)
-        for item in my_read_file:
-            if name == item[0]:
-                print(item)
-                return [line for line in self.read_file() if line[0].lower() == name.lower()]
-            else:
-                break
+        return [line for line in self.read_file() if line[0] == name and line[1] == password]
 
     def __repr__(self):
         return str(self.account)
 
     def write_to_file(self, new_user=None):
+
         with open("login_db", "a") as outfile:
             outfile.write(new_user)
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from db_model import DBReader
 
 
-class Main:
-    def __init__(self, username=None):
-        pass
-
 while True:
     username = input("Please enter your user name: ")
     password = input("Please enter your password: ")
@@ -12,14 +8,22 @@ while True:
     if not user_account.account:
         print("Sorry, Mr. Hacker. That login info is incorrect. Go hack someone else.")
     else:
-        print("Your account: \n" + str(user_account))
+        print("Your account: \n" + str(user_account.account))
         if input("Would you like to create a user or log out? C/l").lower() == 'c':
             new_user = ''
-            new_user += '\n' + str(input("Enter a user name: "))
+            new_user += str(input("Enter a user name: "))
             new_user += ',' + str(input("Enter a password: "))
             new_user += ',' + str(input("Enter your favorite color: "))
-            new_user += ',' + str(input("Enter your favorite food: "))
-            DBReader().write_to_file(new_user)
-            break
+            new_user += ',' + str(input("Enter your favorite food: ") + '\n')
+            for line in DBReader().read_file():
+                if new_user.split(',')[0] == line[0]:
+                    print("Sorry, That User Name Is Already Taken.")
+                    new_user = None
+                    break
+
+
+            if new_user:
+                DBReader().write_to_file(new_user)
+                print("Success! New User Created.")
         else:
             print("Goodbye!")
