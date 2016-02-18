ee3c45f48b5b05decd585f8fe8cc7aa5ec4781b4
jnwcki <jude.nowicki@gmail.com>
2016-02-17 23:22:02 -0500

    completed normal

--- db_model.py
+++ db_model.py
@@ -5,13 +5,24 @@ class DBReader:
         self.password = password
         self.account = self.get_user(name, password)
         self.user_data = None
-        self.status_report = ''
+
     def read_file(self):
         with open("login_db") as infile:
             return [line.split(',') for line in infile.readlines()]
 
+    def filter_by_name(self, name):
+            return [line for line in self.read_file() if line[0].lower() == name.lower()]
+
+    def get_by_name(self, name):
+        results = self.filter_by_name(name)
+        if len(results) > 1:
+            raise Exception("Found more than one record for {}".format(name))
+
+
+
     def get_user(self, name, password):
-        return [line for line in self.read_file() if line[0] == name and line[1] == password]
+        self.get_by_name(name)
+        return [line for line in self.read_file() if line[0].lower() == name.lower() and line[1] == password]
 
     def __repr__(self):
         return str(self.account)
@@ -19,4 +30,4 @@ class DBReader:
     def write_to_file(self, new_user=None):
 
         with open("login_db", "a") as outfile:
-            outfile.write(new_user)
+            outfile.write('\n' + new_user)
--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from db_model import DBReader
 
-
-while True:
-    username = input("Please enter your user name: ")
-    password = input("Please enter your password: ")
-    user_account = DBReader(username, password)
-    if not user_account.account:
-        print("Sorry, Mr. Hacker. That login info is incorrect. Go hack someone else.")
-    else:
-        print("Your account: \n" + str(user_account.account))
-        if input("Would you like to create a user or log out? C/l").lower() == 'c':
-            new_user = ''
-            new_user += str(input("Enter a user name: "))
-            new_user += ',' + str(input("Enter a password: "))
-            new_user += ',' + str(input("Enter your favorite color: "))
-            new_user += ',' + str(input("Enter your favorite food: ") + '\n')
-            for line in DBReader().read_file():
-                if new_user.split(',')[0] == line[0]:
-                    print("Sorry, That User Name Is Already Taken.")
-                    new_user = None
-                    break
-
-
-            if new_user:
-                DBReader().write_to_file(new_user)
-                print("Success! New User Created.")
+def main_login():
+    while True:
+        username = input("Please enter your user name: ")
+        password = input("Please enter your password: ")
+        try:
+            user_account = DBReader(username, password)
+        except:
+            print("More than one account with that username.")
+            main_login()
+        if not user_account.account:
+            print("Sorry, Mr. Hacker. That login info is incorrect. Go hack someone else.")
         else:
-            print("Goodbye!")
+            print("Your account: \n" + str(user_account.account))
+            if input("Would you like to create a user or log out? C/l").lower() == 'c':
+                new_user = ''
+                new_user += str(input("Enter a user name: "))
+                new_user += ',' + str(input("Enter a password: "))
+                new_user += ',' + str(input("Enter your favorite color: "))
+                new_user += ',' + str(input("Enter your favorite food: "))
+                for line in user_account.read_file():
+                    if new_user.split(',')[0] == line[0]:
+                        print("Sorry, That User Name Is Already Taken.")
+                        new_user = None
+                        break
+
+
+                if new_user:
+                    user_account.write_to_file(new_user)
+                    print("Success! New User Created.")
+            else:
+                print("Goodbye!")
+main_login()
